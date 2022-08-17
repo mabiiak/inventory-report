35f4a2613ad06f517cffb957eb6818b140010a2e
mabiiak <mabiane.polniak@gmail.com>
2022-08-17 13:29:24 -0300

    test: testa resultado dos relatorios

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "coquinha",
+        "cola-cola",
+        "18-08-18",
+        "18-09-28",
+        "1556 1561 651 5561",
+        "Manter gelada"
+    )
+
+    assert produto.__repr__() == (
+        "O produto coquinha"
+        " fabricado em 18-08-18"
+        " por cola-cola com validade"
+        " até 18-09-28"
+        " precisa ser armazenado Manter gelada."
+    )
