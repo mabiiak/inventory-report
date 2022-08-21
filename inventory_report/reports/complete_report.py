46a0d23ba3427651c890724d2e3a1a757851b17e
mabiiak <mabiane.polniak@gmail.com>
2022-08-21 20:44:28 -0300

    feat: gera relatorio completo

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,24 @@
+from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def conta_produtos(cls, lista):
+        return Counter(
+            produto["nome_da_empresa"]
+            for produto in lista
+            if produto.get("nome_da_empresa")
+        )
+
+    @staticmethod
+    def generate(lista):
+        relatorio_simples = SimpleReport.generate(lista)
+        relatorio_estoque = CompleteReport.conta_produtos(lista)
+        relatorio_completo = (
+            relatorio_simples + "\n Produtos estocados por empresa:\n"
+        )
+        for key, value in relatorio_estoque.items():
+            relatorio_completo += f"- {key}: {value}\n"
+
+        return relatorio_completo
