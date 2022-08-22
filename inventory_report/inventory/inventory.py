177376cb0d8d0b01e8d9ce01e4b8aa24a4ea6f6d
mabiiak <mabiane.polniak@gmail.com>
2022-08-22 07:44:15 -0300

    feat: salva relatorio em json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,14 +1,16 @@
 from inventory_report.importer.csv_importer import CsvImporter
-from reports.simple_report import SimpleReport
-from reports.complete_report import CompleteReport
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
     def checa_tipo_do_arquivo(caminho):
         if "csv" in caminho:
             return CsvImporter.import_data(caminho)
+        if "json" in caminho:
+            return JsonImporter.import_data(caminho)
 
-    @classmethod
     def import_data(caminho, tipo):
         arquivo = Inventory.checa_tipo_do_arquivo(caminho)
 
