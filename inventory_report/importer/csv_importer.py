d1e55dde9061451b95e87bd36891587da237de1b
mabiiak <mabiane.polniak@gmail.com>
2022-08-22 07:21:50 -0300

    feat: importaçao csv

---
+++ inventory_report/importer/csv_importer.py
@@ -0,0 +1,12 @@
+import csv
+from .importer import Importer
+
+
+class CsvImporter(Importer):
+    @classmethod
+    def import_data(caminho):
+        if ".csv" not in caminho:
+            raise ValueError("Arquivo inválido")
+        with open(caminho) as file:
+            arquivo_csv = list(csv.DictReader(file))
+        return arquivo_csv
